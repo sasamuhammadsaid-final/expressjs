42811b3f066d90ed6dee1c4844f9018f10285865
Sasa Muhammad Said <sasa.said@phincon.com>
2025-05-27 11:58:26 +0700

    handling different routes

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next(); // Allows the request to continue to the next middleware in line
+app.use('/',(req, res, next) => {
+    console.log('this always runs!');
+    next();
 });
 
-app.use((req, res, next) => {
-    console.log('In another middleware!'); 
+app.use('/add-product', (req, res, next) => {
+    console.log("in another middleware!");
+    res.send('<h1>Add Product Page</h1>');
+})
+
+app.use('/', (req, res, next) => {
+    console.log("in another middleware!");
     res.send('<h1>Hello from Express!</h1>');
 });
 
