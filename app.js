b2cc00fd9b7c0485cbcd47b570618796ad1e912b
Sasa Muhammad Said <sasa.said@phincon.com>
2025-06-02 21:15:46 +0700

    bagian 5 - serving files statically

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,6 +7,7 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
