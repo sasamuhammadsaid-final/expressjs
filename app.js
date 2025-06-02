bb269aae3dbda5ea49f44527c3cc5ba27403e75a
Sasa Muhammad Said <sasa.said@phincon.com>
2025-06-02 11:21:24 +0700

    bagian 5 - filtering path

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use('/',(req, res, next) => {
-    console.log('this always runs!');
-    next();
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/add-product', (req, res, next) => {
-    console.log("in another middleware!");
-    res.send('<h1>Add Product Page</h1>');
-})
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/', (req, res, next) => {
-    console.log("in another middleware!");
-    res.send('<h1>Hello from Express!</h1>');
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) =>{
+    res.status(404).send('<h1>Page not found</h1>');
 });
 
 app.listen(3000, () => console.log('listening on port 3000'));
